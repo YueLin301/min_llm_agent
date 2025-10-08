1eeedef2a73bdfbd9b419ac3548e79d648283779
Yue Lin <yue@Mac.lan>
2025-10-08 16:25:39 +0800

    v0

---
+++ bin/lazy_commit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Input a commit message: "
+read commit_message
+git add .
+git commit -m "$commit_message"
+git push origin main
---
+++ draft/some_model_names.py
@@ -0,0 +1,14 @@
+
+# model_name = "gpt-4o-mini"
+# model_name = "gpt-4.1-mini-2025-04-14"
+# model_name = "o3-mini-2025-01-31"
+# model_name = "o3-2025-04-16"
+# model_name = "gpt-5-mini-2025-08-07"
+model_name = "gpt-5-2025-08-07"
+
+
+# model_name = "o1-mini-2024-09-12" # debug
+# model_name = "gpt-4o-2024-11-20"  # debug
+
+# model_name = "deepseek-chat"  # V3
+# model_name = "deepseek-reasoner"  # R1
