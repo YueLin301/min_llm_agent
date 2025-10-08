543ca6c756b7b5876c407104f50ff27fb02d5e61
Yue Lin <yue@Mac.lan>
2025-10-08 18:01:27 +0800

    lazy commit

--- demo/demo1_query_single.py
+++ demo/demo1_query_single.py
@@ -1,9 +1,12 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 from src.min_llm_agent.min_llm_agent import min_llm_agent_class
+# from min_llm_agent import min_llm_agent_class
+
 
 if __name__ == "__main__":
 
@@ -14,6 +17,6 @@ if __name__ == "__main__":
 
     print(f"Question: {question}")
     print(f"Answer by model {llm_agent.model_name}: {response}")
-    
+
     # llm_agent.print_memory(memory_item_separator="/")
     llm_agent.print_memory()
